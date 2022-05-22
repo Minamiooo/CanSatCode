58362e68549ed03eb7cfd412e40f864afdad98f5
aaron-139 <94577369+aaron-139@users.noreply.github.com>
2022-05-22 15:29:21 -0500

    Add files via upload

---
+++ csv_gen.py
@@ -0,0 +1,69 @@
+# CSV Generation for payload and container
+import pandas as pd
+import os.path 
+import csv
+from csv import writer
+
+headerCON = ['TEAM_ID', 'MISSION_TIME', 'PACKET_COUNT','PACKET_TYPE','MODE','TP_RELEASED','ALTITUDE','TEMP','VOLTAGE','GPS_TIME',
+            'GPS_LATITUDE','GPS_LONGITUDE','GPS_ALTITUDE','GPS_SATS','SOFTWARE_STATE','CMD_ECHO']
+
+headerPAY = ['TEAM_ID', 'MISSION_TIME', 'PACKET_COUNT','PACKET_TYPE','TP_ALTITUDE','TP_TEMP','TP_VOLTAGE','GYRO_R','GYRO_P','GYRO_Y','ACCEL_R','ACCEL_P','ACCEL_Y'
+            ,'MAG_R','MAG_P,','MAG_Y','POINTING_ERROR','TP_SOFTWARE_STATE']
+
+# build_csv builds the csv file to store all data in
+def build_csv():
+    
+    if os.path.exists("C:/Users/aarra/Python/CANSAT/Flight_1076C.csv") == False:
+    
+        with open('Flight_1076C.csv','w') as file:
+            writerCON = csv.writer(file, delimiter = ",")
+            writerCON.writerow(headerCON)
+            file.close()
+
+        with open('Flight_1076P.csv','w') as file:
+            writerPAY = csv.writer(file, delimiter = ',')
+            writerPAY.writerow(headerPAY)
+            file.close()
+    else:
+        pass
+
+# build_csv()
+
+# append_csv makes sure the csv files exist then adds data to them as it is received  
+def append_csv(data):
+    
+    if os.path.exists("C:/Users/aarra/Python/CANSAT/Flight_1076C.csv") == True:
+    
+        with open('Flight_1076C.csv','a') as file:
+            writerCON = csv.writer(file)
+            writerCON.writerows(data)
+            file.close()
+        
+        with open('Flight_1076P.csv','a') as file:
+            writerPAY = csv.writer(file)
+            writerPAY.writerows(data)
+            file.close()
+    else:
+        build_csv()
+
+        with open('Flight_1076C.csv','a') as file:
+            writerCON = csv.writer(file, delimiter = ",")
+            writerCON.writerow(data)
+            file.close()
+        
+        with open('Flight_1076P.csv','a') as file:
+            writerPAY = csv.writer(file, delimiter = ',')
+            writerPAY.writerow(data)
+            file.close()
+            
+
+def append_pandas(data):
+    
+    if os.path.exists("C:/Users/aarra/Python/CANSAT/Flight_1076C.csv") == True:
+        dataframe = pd.DataFrame(data)
+        dataframe.to_csv('C:/Users/aarra/Python/CANSAT/Flight_1076C.csv', mode='a', index=False, header=False)
+    else:
+        build_csv()
+        dataframe = pd.DataFrame(data)
+        dataframe.to_csv('C:/Users/aarra/Python/CANSAT/Flight_1076C.csv', mode='a', index=False, header=False )
+        pass
